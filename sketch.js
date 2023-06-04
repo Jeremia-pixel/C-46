09e79eb132fe6ed31043793cbcafd2fa783db65f
Jeremia-pixel <jeremiapious7a@gmail.com>
2023-06-04 15:18:47 +0530

    adding obstacles

--- sketch.js
+++ sketch.js
@@ -1,57 +1,132 @@
 var human,humanImg;
 var invisibleGround;
 var ground;
-var tap, tapImg;
+var obstaclesGroup1, obstaclesGroup2;
+var obstacle1Image, obstacle2Image;
+var humanRunning;
+var rotationAngle = 0;
+var gameState = -1;
 
 function preload(){
-  tapImg = loadImage("tap.png");
-  humanImg = loadAnimation("human1.png","human2.png","human3.png","human4.png","human5.png");
+  humanImg = loadAnimation("human2.png");
+  humanRunning = loadAnimation("human1.png","human2.png","human3.png","human4.png","human5.png");
+  obstacle1Image = loadImage("obstacle1.png");
+  obstacle2Image = loadImage("obstacle2.png");
 }
 
 function setup() {
-  createCanvas(1600,400);
+  createCanvas(1500,400);
   human = createSprite(100, 200, 50, 50);
-  human.addAnimation("running",humanImg);
+  human.addAnimation("walk",humanImg);
+  human.addAnimation("running",humanRunning);
+  human.debug = false;
 
   invisibleGround = createSprite(800,350,1800,25);
   invisibleGround.visible = false;
   
   ground = createSprite(800,350,1800,25);
+  ground.debug = false;
+  
 
-  tap = createSprite(480,280,35,35);
-  tap.addImage(tapImg);
-  tap.scale = 0.35;
+
+  obstaclesGroup1 = createGroup();
+  obstaclesGroup2 = createGroup();
+
 }
 
 function draw() {
-  background(255,255,255);  
+  background("blue");  
+  //console.log(gameState);
+  if(gameState === 1){
+    if(human.isTouching(invisibleGround)){
+      human.collide(invisibleGround)
+    }
   
-  if(human.isTouching(invisibleGround)){
-    human.collide(invisibleGround)
+    
+      human.changeAnimation("running",humanRunning);
+      ground.velocityX = -5;
+      
+    
+  
+    if(human.y>290&&keyWentDown("Space")){
+      human.velocityY = -10
+    }
+    console.log(human.x);
+  
+    if(ground.x < ground.width/8){
+      ground.x = 400;
+    }
+  
+    //camera.position.x = human.x;
+  
+    human.velocityY = human.velocityY+0.5;
+    rotationAngle +=10;
+    obstaclesGroup1.setRotationEach(rotationAngle);
+    obstaclesGroup1.collide(ground);
+    obstaclesGroup2.collide(ground);
+    if(frameCount%90==0){
+      var ran = Math.round(random(1,2))
+      switch(ran){
+        case 1: 
+        spawnObstacles1();
+        break;
+        case 2:
+        spawnObstacles2();
+        break;
+      }
+    }
   }
 
-  if(keyDown("RIGHT")){
-    human.velocityX = 6
-  }
-
-  if(human.y>290&&keyWentDown("UP")){
-    human.velocityY = -10
-  }
-  console.log(human.x);
-  
-  if(human.x === 400){
-    human.velocityX = 0;
+  if(gameState === 0){
+    strokeWeight(3)
+    stroke("red")
+    textSize(25)
+    fill("yellow")
+    text("Game Over", 750,200);
+    obstaclesGroup1.setVelocityEach(0,0);
+    obstaclesGroup2.setVelocityEach(0,0);
+    ground.velocityX = 0;
+    human.changeAnimation("walk",humanImg);
     human.velocityY = 0;
-    text("Press Space To Close The Tap",450,125);
-    text("Or Press The Right Arrow To Continue",450,160);
   }
+if(obstaclesGroup1.isTouching(human)|| obstaclesGroup2.isTouching(human)){
+  gameState = 0;
+}
+if(gameState === -1){
+  stroke("green")
+  fill("red")
+  textSize(25)
+  text("Press Up Arrow to Begin", 750,200);
+}
+if(keyWentDown("UP")&& gameState === -1){
+  gameState = 1;
+  human.x = 100;
 
-  camera.position.x = human.x;
-
-  human.velocityY = human.velocityY+0.5;
-
-
+}
   drawSprites();
 
   
+}
+function spawnObstacles1(){
+  //if(frameCount%90==0){
+    var obstacle = createSprite(1500,300,100,100);
+    obstacle.setCollider("circle",0,0,5);
+    obstacle.debug = true;
+    obstacle.scale=0.4
+  obstacle.velocityX = -1*Math.round(random(4,10));
+  obstacle.addImage(obstacle1Image);
+  obstaclesGroup1.add(obstacle)
+  //}
+}
+
+function spawnObstacles2(){
+ // if(frameCount%90==0){
+    var obstacle = createSprite(1500,300,100,100);
+    obstacle.setCollider("circle",0,0,5);
+    obstacle.debug = true;
+    obstacle.scale=0.4
+  obstacle.velocityX = -1*Math.round(random(4,10));
+  obstacle.addImage(obstacle2Image);
+  obstaclesGroup2.add(obstacle)
+ // }
 }
